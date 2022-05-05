478b899745e988d7f574e3891f32752c3beaf0d6
XCA Student <xcastudent@Admins-MBP.attlocal.net>
2022-05-04 22:29:01 -0400

    added error handling

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
 const express = require('express');
-const data = require('./data');
+const { projects } = require('./data.json');
+console.log(projects);
 // const index = require('./routes/index');
 // const about = require('./routes/about');
 
 const app = express();
 app.set('view engine', 'pug');
 
+// app.use('/error', (req, res, next) => {
+//     console.log("Hello")
+//     const err = new Error()
+//     err.message = 'Custom 505'
+//     err.status = 500
+//     throw err
+// })
+
 // Route for static files
 app.use('/static', express.static('public'));
 
 // app.use(index);
-// app.use(about);
-
-
 app.get('/', (req, res) => {
-    res.render('index', data.projects);
+    res.render('index', { projects });
 });
-
+// app.use(about);
 app.get('/about', (req, res) => {
     res.render('about');
 });
-
-app.get('/projects/:id', (req, res) => {
-
+// app
+app.get('/project/:id', (req, res, next) => {
+    const id  = req.params.id;
+    if (projects[id]) {
+        res.render('project', { project: projects[id] })
+    } else {
+        const err = new Error()
+        err.status = 404
+        err.message = 'This page does not exist'
+        next(err)
+    }
+    
 });
 
+app.use((req, res, next) => {
+    const err = new Error("This page does not exist")
+    err.status = 404
+    console.log(`Oops there has been a ${err.status} error. ${err.message}`)
+    next(err)
+})
+
+
+app.use(( err, req, res, next ) => {
+    res.locals.error = err;
+    if (err.status === 404) {
+      res.status(err.status);
+      res.render('page-not-found')
+    } else {
+        err.message = `OOPs! Something went wrong on the server!`
+        res.status(err.status || 500);
+        res.render('error', err);
+  }
+});
 
 
 app.listen(3000, () => {
