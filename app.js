e7212a8124c9aa9c1495cb657a8232906d3336fe
XCA Student <xcastudent@Admins-MBP.attlocal.net>
2022-09-23 09:53:54 -0400

    changes to app.js

--- app.js
+++ app.js
@@ -1,47 +1,18 @@
 const express = require('express');
-<<<<<<< HEAD
-const data = require('./data');
-// const index = require('./routes/index');
-// const about = require('./routes/about');
-=======
 const bodyParser = require('body-parser')
 const { projects } = require('./data.json');
 // console.log(projects);
->>>>>>> 7904f82466c25d8cc03363cc321daeabba053150
 
 const app = express();
 app.set('view engine', 'pug');
 
-<<<<<<< HEAD
-=======
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
->>>>>>> 7904f82466c25d8cc03363cc321daeabba053150
 // Route for static files
 app.use('/static', express.static('public'));
 
 // app.use(index);
-<<<<<<< HEAD
-// app.use(about);
-
-
-app.get('/', (req, res) => {
-    res.render('index', data.projects);
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/projects/:id', (req, res) => {
-
-});
-
-
-
-app.listen(3000, () => {
-=======
 app.get('/', (req, res) => {
     res.render('index', { projects });
 });
@@ -86,6 +57,5 @@ app.use(( err, req, res, next ) => {
 
 
 app.listen(process.env.PORT || 3000, () => {
->>>>>>> 7904f82466c25d8cc03363cc321daeabba053150
     console.log('This application is running on localhost:3000')
 });
